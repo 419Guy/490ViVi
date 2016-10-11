2ed390965756164bc1952070c61e2f3919c0f586
aiLim003 <al322@njit.edu>
2016-10-11 09:06:57 -0400

    Update logout.php
    Edited Logout.php to fix tags and added link back to original login form

--- logout.php
+++ logout.php
@@ -1,8 +1,11 @@
 <html>
-	<head>But sir, you forgot you hat.<head>
+	<head>But sir, you forgot you hat.</head>
 	<body>
 
 <?php
 		session_start();
 		session_destroy();
 ?>
+		<a href=index.html>Click here to return to login page</a>
+	</body>
+		</html>
