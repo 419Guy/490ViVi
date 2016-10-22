b43b5dcc54a48c4d68eb3c970d1e48c7b7c11400
nkh96 <nkh6@njit.edu>
2016-10-22 15:23:38 -0400

    Create proxy_config.php

---
+++ Open-Textbooks/includes/proxy_config.php
@@ -0,0 +1,14 @@
+<?php
+
+//Proxies are necessary to bypass Follets and Barnes and Nobles rate limiting.
+//Services I recommend for proxies.  Low-traffic: proxymesh, AWS cloud, or rackspace cloud.  High-traffic: trustedproxies, hide my ass
+
+//NOTE: Make sure to specify the Port in addition to host
+
+define('PROXY_1', '104.238.189.97');
+define('PROXY_1_AUTH', '31280');
+
+define('PROXY_2', '45.32.11.96');
+define('PROXY_2_AUTH', '31280');
+
+?>
