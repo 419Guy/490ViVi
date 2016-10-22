614ff716935585282651fd8db7027627decba129
nkh96 <nkh6@njit.edu>
2016-10-22 15:16:47 -0400

    Create db_config.php

---
+++ Open-Textbooks/includes/db_config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Fill this in with your MySQL database settings
+
+define("DB_HOST", "localhost");
+define("DB_USERNAME", "root@localhost");
+define("DB_PASSWORD", "usawari1");
+define("DB_NAME", "it490textdb");
+
+?>
