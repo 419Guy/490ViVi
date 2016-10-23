93ff9cf61644d88b8e601592937e86f5ca6e55ca
aiLim003 <al322@njit.edu>
2016-10-23 16:08:31 -0400

    Update loginmq
    Fixed code so that RabbitMQ works directly with browser.  Make sure the php.ini file in apache2 has "extension=amqp.so" in it so that this works.

--- loginmq.php
+++ loginmq.php
@@ -1,4 +1,4 @@
-#!/usr/bin/php
+
 <?php
 require_once('path.inc');
 require_once('get_host_info.inc');
@@ -7,16 +7,23 @@ require_once('rabbitMQLib.inc');
 $client = new rabbitMQClient("testRabbitMQ.ini","testServer");
 
 	$request = array();
-	$request['username'] = $argv[1];
-	$request['password'] = $argv[2];
+	$request['username'] = $_POST['username'];
+        $request['password'] = $_POST['password'];
+
 	
 	$response = $client->send_request($request);
-	echo "client received response: ". PHP_EOL;
-	
-//	session_start();
-  //      $_SESSION['login'] = "1";
-    //    header ("Location:postlogin.php");
+	//echo "client received response: ". PHP_EOL;
+	//var_dump($response);
 
-	
+	if($response['returnCode'] == 0){
+		 session_start();
+                $_SESSION['login'] = "1";
+                header ("Location:postlogin.php");
+		//redirect to postlogin.php if user logged in
+		
+	}else{
+
+		echo "Sorry, wrong password.  Please try again.";
+	}
+		
 ?>
-
