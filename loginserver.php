1ba3167a918cd89a5d8f10897a8749ff7e17b1a5
aiLim003 <al322@njit.edu>
2016-10-23 16:14:04 -0400

    Update loginserver
    Server connects to MySQL database with the credentials from client.

--- loginserver.php
+++ loginserver.php
@@ -4,7 +4,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function doLogin($username,$password)
+function doLogin($username,$password) //username and password come from client
 {
     // lookup username in database
     // check password
@@ -16,16 +16,17 @@ function doLogin($username,$password)
         }
 	$sql = "select * from register where username = '$username' AND password = sha2('$password', 256)";
 	 if(!$result = $db->query($sql)){
-                die ('There was an error running the query [' . $db->error . ']');
+                die ('There was an error running the query [' . $db->error . ']'); //will state error in query
 }
 	$count = $result->num_rows;
 
         if ($count == 0){
         echo"Sorry, wrong password.  Please try again.";
-	shell_exec("php -q /var/www/demo/connect.mq $count");
+	return false;
         }else{
 	echo " Thank you for logging in/n";
 	$query = "update register set login_time = NOW() where username = '$username' AND password = sha2('$password', 256)";
+		//we know what time and day user has logged in
                 if(!$result2 = $db->query($query)){
                  die ('There was an error running the query [' . $db->error . ']');
         }
@@ -34,9 +35,11 @@ function doLogin($username,$password)
 		
 		while ($row = $result->fetch_assoc())
 		{
+			//loop will print in server the information from database of that user
    		print_r($row);
-		}		
-	return true;
+		}
+	return true;		
+
 
 	}	
 }
@@ -45,10 +48,13 @@ function requestProcessor($request)
 
   echo "received request".PHP_EOL;
   var_dump($request);
-doLogin($request['username'], $request['password']); 
- return array("returnCode" => '0', 'message'=>"Server received request and processed");
+	$result = doLogin($request['username'], $request['password']); 
+	if($result == true){
+	return array("returnCode" => '0', 'message'=>"Server received request and processed"); //sending back to client
+	}else{
+		return array("returnCode" => '1', 'message'=>"Server received request and processed"); //sending back to client
+	}
 }
-
 $server = new rabbitMQServer("testRabbitMQ.ini","testServer");
 
 $server->process_requests('requestProcessor');
