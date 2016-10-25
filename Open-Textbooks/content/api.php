8c5eb247461d34f569e9fdecdcd6f3454dc80667
nkh96 <nkh6@njit.edu>
2016-10-25 15:45:49 -0400

    Add files via upload

--- Open-Textbooks/content/api.php
+++ Open-Textbooks/content/api.php
@@ -8,11 +8,12 @@
 // api.php?campus=N&term=N&division=N&dept=N&course=N
 // api.php?campus=N&term=N&division=N&dept=N&course=N&section=N
 //
-//..where N is an ID.  Note that the division always has to be provided. even if the system in question doesn't have divisionas, the bookstore_functions code creates a placeholder
-
-require_once('../includes/autoloads.php');
-
-date_default_timezone_set('GMT');
+//..where N is an ID.  Note that the division always has to be provided. even if the system in question doesn't have divisions, the bookstore_functions code creates a placeholder
+//changed the timezone
+//added i at the end of mysql
+//added $conn before $query
+require_once(__DIR__.'/../includes/autoloads.php');
+date_default_timezone_set('EST');
 
 $first_start = microtime(true);
 
@@ -23,6 +24,7 @@ $json['data'] = array();
 
 if (!$conn = connect())
 {
+	echo "DB CONNECT FAIL";	
 	$json['status'] = 'Error: DB Connect failure';
 }
 
@@ -41,20 +43,24 @@ else if (!isset($_GET['campus']))
 	WHERE Campus_Names.Is_Primary = "Y"
 	ORDER BY Campus_Names.Campus_Name ASC';
 	
-	if (!$result = mysql_query($query))
+	if (!$result = mysqli_query($conn, $query))
 	{
-		$json['status'] = 'Error: SQL query for all campuses yielded error: '. mysql_error();
+		echo "all yielded error";
+		$json['status'] = 'Error: SQL query for all campuses yielded error: '. mysqli_error($conn);
 	}
-	else if (mysql_num_rows($result) == 0)
+	else if (mysqli_num_rows($result) == 0)
 	{
+		echo "no results";
 		$json['status'] = 'Error: SQL query for all campuses yielded no results';
 	}
 	else //all good, show them the campuses
 	{
+		echo "cool\n";
 		$json['status'] = 'ok';
-		while ($row = mysql_fetch_assoc($result))
+		while ($row = mysqli_fetch_assoc($result))
 		{
-			$json['data'][] = array('id' => $row['Campus_ID'], 'name' => $row['Campus_Name']);
+		//	print_r($row);
+			$json['data'] = array('id' => $row['Campus_ID'], 'name' => $row['Campus_Name']);//there was a double bracket [] after data we took it out if something is missing it might be this
 		}
 	}
 }
@@ -96,11 +102,11 @@ else if (!isset($_GET['section']))
 		$end = microtime(true);
 		$json['first_query_time'] = round($end - $start, 3) * 1000;
 		
-		if (!$result = mysql_query($query))
+		if (!$result = mysqli_query($conn, $query))
 		{
-			$json['status'] = 'Error: SQL query based on '. $sent_name .'='. $sent_val .' yielded error: '. mysql_error();
+			$json['status'] = 'Error: SQL query based on '. $sent_name .'='. $sent_val .' yielded error: '. mysqli_error($conn);
 		}
-		else if (mysql_num_rows($result) == 0)
+		else if (mysqli_num_rows($result) == 0)
 		{
 			$json['status'] = 'Error: SQL query based on '. $sent_name .'='. $sent_val .' yielded no results';
 		}
@@ -109,17 +115,17 @@ else if (!isset($_GET['section']))
 			$next_name = $names_array[$sent_dd + 1];
 			$next_id = $ids_array[$sent_dd + 1];
 		
-			$row = mysql_fetch_assoc($result);
+			$row = mysqli_fetch_assoc($result);
 			
 			if (!$row[$next_id])
 			{
 				$start = microtime(true);
-				mysql_close($conn);
+				mysqli_close($conn);
 				
 				update_classes_from_bookstore($row); //make bookstore requests to update db cache with what we want.. note that this attempts 3 times..
 				if (!$conn = connect())
 				{
-					$json['status'] = mysql_error();
+					$json['status'] = mysqli_error($conn);
 					die(json_encode($json));
 				}
 				$end = microtime(true);
@@ -127,11 +133,11 @@ else if (!isset($_GET['section']))
 				
 				//now we try to the query again..
 				$start = microtime(true);
-				if (!$result = mysql_query($query))
+				if (!$result = mysqli_query($conn, $query))
 				{
-					$json['status'] = 'Error: SQL query based on '. $sent_name .'='. $sent_val .' yielded error: '. mysql_error();
+					$json['status'] = 'Error: SQL query based on '. $sent_name .'='. $sent_val .' yielded error: '. mysqli_error($conn);
 				}
-				else if (mysql_num_rows($result) == 0)
+				else if (mysqli_num_rows($result) == 0)
 				{
 					$json['status'] = 'Error: SQL query based on '. $sent_name .'='. $sent_val .' yielded no results';
 				}
@@ -140,10 +146,10 @@ else if (!isset($_GET['section']))
 			}
 			else
 			{	
-				mysql_data_seek($result, 0); //rewind
+				mysqli_data_seek($result, 0); //rewind
 			}
 			
-			$row = mysql_fetch_assoc($result);
+			$row = mysqli_fetch_assoc($result);
 			if (!$row[$next_id])
 			{
 				$json['status'] = 'Warning: No entities returned from bookstore with '. $sent_name .'='. $sent_val . '. Could be scraping error, or there could genuinely be no corresponding entities.';
@@ -151,11 +157,11 @@ else if (!isset($_GET['section']))
 			else //everything worked out; time to return ata
 			{
 				$json['status'] = 'ok';
-				mysql_data_seek($result, 0); //rewind
+				mysqli_data_seek($result, 0); //rewind
 				
 				$data = array();
 				
-				while ($row = mysql_fetch_assoc($result))
+				while ($row = mysqli_fetch_assoc($result))
 				{
 					$data[$row[$next_id]] = array('id' => $row[$next_id], 'name' => $row[$next_name]);
 					
@@ -190,51 +196,51 @@ else
 		
 		$query = class_items_query(array($section_id), $campus_id);
 		
-		if (!$result = mysql_query($query))
+		if (!$result = mysqli_query($conn, $query))
 		{
-			$json['status'] = 'Error: Class-Items SQL query failed with '. mysql_error();
+			$json['status'] = 'Error: Class-Items SQL query failed with '. mysqli_error($conn);
 		}
-		else if (mysql_num_rows($result) == 0)
+		else if (mysqli_num_rows($result) == 0)
 		{
 			$json['status'] = 'Error: Class-Items SQL query yielded no results';
 		}
 		else
 		{	
-			$row = mysql_fetch_assoc($result);
+			$row = mysqli_fetch_assoc($result);
 			if ($row['no_class_item']) //note that this is Class_Items_Cache.Class_ID, NOT Classes_Cache.Class_ID which must have been set from before (or else you'd get the 0 rows error above)
 			{	
-				mysql_close($conn);
+				mysqli_close($conn);
 				update_class_items_from_bookstore(array($row)); //this function updates the DB with the class-item data
 				if (!$conn = connect())
 				{
-					$json['status'] = mysql_error();
+					$json['status'] = mysqli_error($conn);
 					die(json_encode($json));
 				}
-				if (!$result = mysql_query($query))
+				if (!$result = mysqli_query($conn, $query))
 				{
-					$json['status'] = 'Error: Class-Items SQL query failed with '. mysql_error();
+					$json['status'] = 'Error: Class-Items SQL query failed with '. mysqli_error($conn);
 				}
-				else if (mysql_num_rows($result) == 0)
+				else if (mysqli_num_rows($result) == 0)
 				{
 					$json['status'] = 'Error: Class-Items SQL query yielded no results';
 				}
 			}
 			else
 			{
-				mysql_data_seek($result, 0); //rewind
+				mysqli_data_seek($result, 0); //rewind
 			}
 			
-			if (mysql_num_rows($result)) //everything has worked..
+			if (mysqli_num_rows($result)) //everything has worked..
 			{
 				$json['status'] = 'ok';
 				$json['data']['items'] = array();
 				
-				$row = mysql_fetch_assoc($result);
+				$row = mysqli_fetch_assoc($result);
 				
 				if ($row['Item_ID']) //it has at least one item
 				{
 					//So, loop the books into the array
-					while ($row = mysql_fetch_assoc($result))
+					while ($row = mysqli_fetch_assoc($result))
 					{
 						$Books = load_books_from_row($Books, $row);
 					}
@@ -242,11 +248,11 @@ else
 					//Load our ish into JSON and output...
 					$data = array('items' => array());
 			
-					mysql_data_seek($result, 0);
+					mysqli_data_seek($result, 0);
 					
 					$previous_item = false; //keeps track of whether we've run into a new time
 					
-					while ($row = mysql_fetch_assoc($result))
+					while ($row = mysqli_fetch_assoc($result))
 					{
 						if ($row['Item_ID'] != $previous_item)
 						{
@@ -266,6 +272,5 @@ else
 
 $json['total_time'] = round(microtime(true) - $first_start,3) * 1000;
 
-echo json_encode($json);
-
+print_r(json_encode($json));
 ?>
