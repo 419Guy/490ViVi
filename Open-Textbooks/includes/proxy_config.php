d626fa5126a40d1b251c654ac74407e6833173b1
nkh96 <nkh6@njit.edu>
2016-10-24 22:00:03 -0400

    Update proxy_config.php

--- Open-Textbooks/includes/proxy_config.php
+++ Open-Textbooks/includes/proxy_config.php
@@ -4,7 +4,8 @@
 //Services I recommend for proxies.  Low-traffic: proxymesh, AWS cloud, or rackspace cloud.  High-traffic: trustedproxies, hide my ass
 
 //NOTE: Make sure to specify the Port in addition to host
-
+//We're using a free trial to use these proxies note: go back in a month to make a new account and just change the proxies.
+// Specically probably sometime in the middle of November
 define('PROXY_1', '104.238.189.97');
 define('PROXY_1_AUTH', '31280');
 
