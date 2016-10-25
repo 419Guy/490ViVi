d9b8992392472e4071359194573c4c89b5fb447a
nkh96 <nkh6@njit.edu>
2016-10-24 21:57:49 -0400

    Update db_config.php

--- Open-Textbooks/includes/db_config.php
+++ Open-Textbooks/includes/db_config.php
@@ -1,7 +1,7 @@
 <?php
 
 //Fill this in with your MySQL database settings
-
+//It has been filled with the database we're using information 
 define("DB_HOST", "localhost");
 define("DB_USERNAME", "root@localhost");
 define("DB_PASSWORD", "usawari1");
