a1d1fa3f95ad2d5a2ba73a2f5e16ceb29f95d9ee
aiLim003 <al322@njit.edu>
2016-11-07 20:07:00 -0500

    Add files via upload
    Fix price

--- searchserver.php
+++ searchserver.php
@@ -8,22 +8,32 @@ require_once('rabbitMQLib.inc');
 function doBookSearch($title,$author,$isbn13,$publisher,$course,$id)
 {
 
-	$db = new mysqli("localhost","root","khLux2016","project");
+	$db1 = new mysqli("192.168.43.149","dancho","khLux2016","it490");
 
-	if ($db->connect_errno > 0 )
+	if ($db1->connect_errno > 0 )
 	{
-        	echo __FILE__.__Line__." ERROR: ".$db->connect_error.PHP_EOL;
+        	echo __FILE__.__Line__." ERROR: ".$db1->connect_error.PHP_EOL;
        		 exit(-1);
 	}
 
-		$title = $db->real_escape_string($title);
-		$author = $db->real_escape_string($author);
-		$isbn13 = $db->real_escape_string($isbn13);
-		$publisher = $db->real_escape_string($publisher);
-		$course = $db->real_escape_string($course);
-		$loggedin_id = $db->real_escape_string($id);	
+		$title = $db1->real_escape_string($title);
+		$author = $db1->real_escape_string($author);
+		$isbn13 = $db1->real_escape_string($isbn13);
+		$publisher = $db1->real_escape_string($publisher);
+		$course = $db1->real_escape_string($course);
+		$loggedin_id = $db1->real_escape_string($id);	
 	
 		$word_list = array("$title", "$author", "$isbn13", "$publisher");	 
+	//create new database connection for my search table
+
+	 $db = new mysqli("localhost","root","khLux2016","project");
+
+        if ($db->connect_errno > 0 )
+        {
+                echo __FILE__.__Line__." ERROR: ".$db->connect_error.PHP_EOL;
+                 exit(-1);
+        }
+
  		if($loggedin_id != 0){
 
 			foreach ($word_list as $value){
@@ -51,9 +61,9 @@ function doBookSearch($title,$author,$isbn13,$publisher,$course,$id)
 		if($publisher !=""){
         		$conditions[] = "`Publisher` LIKE '%".$publisher."%'";
 		}
-		if($course != ""){
-       			 $conditions[] = "`course` LIKE '%".$course."%'";
-		}
+		//if($course != ""){
+       		//	 $conditions[] = "`course` LIKE '%".$course."%'";
+		//}
 
 		$sql = $query;
 		if (count($conditions) > 0){
@@ -62,8 +72,8 @@ function doBookSearch($title,$author,$isbn13,$publisher,$course,$id)
 
 		echo "This is query: $sql\n";
 
-        	if (!$result = $db->query($sql)){
-        		die ('There was an error running the query [' . $db->error . ']');
+        	if (!$result = $db1->query($sql)){
+        		die ('There was an error running the query [' . $db1->error . ']');
 		}
 		$result_array = array();
 
@@ -73,8 +83,8 @@ function doBookSearch($title,$author,$isbn13,$publisher,$course,$id)
             while($row = $result->fetch_assoc()){
             // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
 	
-                $result_array[] = "<p><h3>".$row['Title']."</h3>Author: ".$row['Authors']."<br>Edition: ".$row['Edition']."<br>Year: ".$row['Year']."<br>ISBN-13: ".$row['ISBN13']."<br>Publisher: ".$row['Publisher']."<br></p>";
-                //echo"<p>NJIT Bookstore Buy Price: $".$row['buy_book']."<br>NJIT Bookstore Rent Price: $".$row['rent_book']."</p>";
+                $result_array[] = "<p><h3>".$row['Title']."</h3>Author: ".$row['Authors']."<br>Edition: ".$row['Edition']."<br>Year: ".$row['Year']."<br>ISBN-13: ".$row['ISBN13']."<br>Publisher: ".$row['Publisher']."<br></p>".
+                "<p>NJIT Bookstore Buy New Price: $".$row['New']."<br>NJIT Bookstore Buy Used Price: $".$row["Used"]."<br>NJIT Bookstore Rent Price: $".$row['Rent']."</p>"."<br><p>Amazon Price: <a href=\"".$row['Amazon']."\">Click here</a>";
                 // posts results gotten from database(title and text) you can also show id ($results['id'])
             }
 		print_r($result_array);
